4e31901e6fefa618e533fe36781c8cc0e167026e
lesak <tripsimon3@gmail.com>
2022-09-27 10:59:28 +0200

    Oprava repozitáře

---
+++ Client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+    >
+      <v-list>
+        <v-list-item
+            title="Rozděl a Panuj"
+            subtitle="Dashboard"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item to="/tvorba-dobrodruha" prepend-icon="mdi-newspaper-variant" title="Tvorba nového dobrodruha" value="myfiles"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Rozděl a Panuj</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+
+<script>
+export default {
+  data: () => ({ drawer: null }),
+}
+</script>
---
+++ Client/src/components/pages/TvorbaDobrodruhaPage.vue
@@ -0,0 +1,45 @@
+
+<template>
+
+<v-container>
+  <h1>Tvorba dobrodruha</h1>
+  <v-divider></v-divider>
+
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+    >
+
+    <v-text-field label="Jméno dobrodruha"></v-text-field>
+
+    <v-select :items="races" label="Rasa"></v-select>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+          valid: true,
+          races: ['Anhilarský člověk', 'Grobr',  'Hruurský člově', 'Ork', 'Inu-im'], //TODO: Backend
+          checkbox: false,
+        }),
+    
+        methods: {
+          validate () {
+            this.$refs.form.validate()
+          },
+          reset () {
+            this.$refs.form.reset()
+          },
+          resetValidation () {
+            this.$refs.form.resetValidation()
+          },
+        },
+      }
+</script>
+
+<style>
+
+</style>
