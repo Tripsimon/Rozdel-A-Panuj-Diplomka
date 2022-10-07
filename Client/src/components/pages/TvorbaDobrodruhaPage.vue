897c873f3e8a3eac7cdb9bdf7a359572f275c1d3
lesak <tripsimon3@gmail.com>
2022-10-07 13:42:00 +0200

    Oprava formuláře

--- Client/src/components/pages/TvorbaDobrodruhaPage.vue
+++ Client/src/components/pages/TvorbaDobrodruhaPage.vue
@@ -17,14 +17,36 @@
     <v-select :items="races" label="Rasa"></v-select>
     <v-select :items="classes" label="Povolání"></v-select>
     <v-select :items="['LMAO','YeetA']" label="Přesvědčení"></v-select>
+    <v-text-field label="Věk"></v-text-field>
     <v-btn-group>
       <v-btn> Jou</v-btn>
       <v-btn> Hou</v-btn>
     </v-btn-group>
 
     <h2>Atributy</h2>
+    <v-text-field label="Síla"></v-text-field>
+    <v-text-field label="Zručnost"></v-text-field>
+    <v-text-field label="Výdrž"></v-text-field>
+    <v-text-field label="Inteligence"></v-text-field>
+    <v-text-field label="Vědění"></v-text-field>
+    <v-text-field label="šarm"></v-text-field>
 
+    <h2>Výbava //TODO</h2>
 
+    <h2>Dodatečné informace</h2>
+    <v-textarea
+          name="input-7-1"
+          label="Další schopnosti"
+          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+          hint="Hint text"
+        ></v-textarea>
+
+        <v-textarea
+          name="input-7-1"
+          label="Příběh"
+          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+          hint="Hint text"
+        ></v-textarea>
 
     </v-form>
   </v-container>
