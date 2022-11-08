e2fe67c2922fae0cc41bfa1dcd9ff7bef9eee4fc
Martin Lešák <47338791+Tripsimon@users.noreply.github.com>
2022-11-08 07:09:22 +0100

    Update TvorbaDobrodruhaPage.vue
    Upravy formuláře

--- Client/src/components/pages/TvorbaDobrodruhaPage.vue
+++ Client/src/components/pages/TvorbaDobrodruhaPage.vue
@@ -3,31 +3,30 @@
 
   <v-container>
     <h1>Tvorba dobrodruha</h1>
-    <v-divider></v-divider>
+    <v-divider color="secondary"></v-divider>
 
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <h2>Základní informace</h2>
+    <v-form ref="form" v-model="valid" lazy-validation >
+      <h2>Pilíře Dobrodruha</h2>
       <v-text-field label="Jméno dobrodruha"></v-text-field>
 
-
-      <v-select :items="races" label="Rasa"></v-select>
+      <v-select color="primary" :items="races" label="Rasa"></v-select>
       <v-select :items="classes" label="Povolání"></v-select>
-      <v-select :items="['LMAO', 'YeetA']" label="Přesvědčení"></v-select>
-      <v-text-field label="Věk"></v-text-field>
 
-      <h2>Atributy</h2>
+
+      <h2>Atributy dobrodruha</h2>
       <v-row>
         <v-col ols="12" sm="6">
-          <v-card title="Síla">
-            <v-container class="grey lighten-5" color="primary">
+          <v-card title="Síla" align="center"  color="primary">
+            <v-divider color="secondary"></v-divider>
+            <v-container class="grey lighten-5" >
               <v-row>
-                <v-col align="center" justify="center" cols="12" sm="4">
-                  <v-btn mb="4" size="small" variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
+                <v-col  cols="12" sm="4">
+                  <v-btn  mb="4" size="medium" variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
                 </v-col>
                 <v-col align="center" justify="center" cols="12" sm="4"><span mb="4"
                     class="text-h2 font-weight-light mb-4" v-text="atributy"></span></v-col>
                 <v-col align="center" justify="center" cols="12" sm="4">
-                  <v-btn mb="4" size="small " variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
+                  <v-btn mb="4" size="medium " variant="text" icon="mdi-plus" :color="color" @click="decrement"></v-btn>
                 </v-col>
               </v-row>
             </v-container>
@@ -35,16 +34,93 @@
         </v-col>
 
         <v-col ols="12" sm="6">
-          <v-card title="Síla" color="secondary">
+          <v-card title="Houževnatost" align="center"  color="primary">
+            <v-divider color="secondary"></v-divider>
             <v-container class="grey lighten-5" >
               <v-row>
-                <v-col align="center" justify="center" cols="12" sm="4">
-                  <v-btn mb="4" size="small" variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
+                <v-col  cols="12" sm="4">
+                  <v-btn  mb="4" size="medium" variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
                 </v-col>
                 <v-col align="center" justify="center" cols="12" sm="4"><span mb="4"
                     class="text-h2 font-weight-light mb-4" v-text="atributy"></span></v-col>
                 <v-col align="center" justify="center" cols="12" sm="4">
-                  <v-btn mb="4" size="small " variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
+                  <v-btn mb="4" size="medium " variant="text" icon="mdi-plus" :color="color" @click="decrement"></v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col ols="12" sm="6">
+          <v-card title="Obratnost" align="center"  color="primary">
+            <v-divider color="secondary"></v-divider>
+            <v-container class="grey lighten-5" >
+              <v-row>
+                <v-col  cols="12" sm="4">
+                  <v-btn  mb="4" size="medium" variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
+                </v-col>
+                <v-col align="center" justify="center" cols="12" sm="4"><span mb="4"
+                    class="text-h2 font-weight-light mb-4" v-text="atributy"></span></v-col>
+                <v-col align="center" justify="center" cols="12" sm="4">
+                  <v-btn mb="4" size="medium " variant="text" icon="mdi-plus" :color="color" @click="decrement"></v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+
+        <v-col ols="12" sm="6">
+          <v-card title="Charisma" align="center"  color="primary">
+            <v-divider color="secondary"></v-divider>
+            <v-container class="grey lighten-5" >
+              <v-row>
+                <v-col  cols="12" sm="4">
+                  <v-btn  mb="4" size="medium" variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
+                </v-col>
+                <v-col align="center" justify="center" cols="12" sm="4"><span mb="4"
+                    class="text-h2 font-weight-light mb-4" v-text="atributy"></span></v-col>
+                <v-col align="center" justify="center" cols="12" sm="4">
+                  <v-btn mb="4" size="medium " variant="text" icon="mdi-plus" :color="color" @click="decrement"></v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col ols="12" sm="6">
+          <v-card title="Inteligence" align="center"  color="primary">
+            <v-divider color="secondary"></v-divider>
+            <v-container class="grey lighten-5" >
+              <v-row>
+                <v-col  cols="12" sm="4">
+                  <v-btn  mb="4" size="medium" variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
+                </v-col>
+                <v-col align="center" justify="center" cols="12" sm="4"><span mb="4"
+                    class="text-h2 font-weight-light mb-4" v-text="atributy"></span></v-col>
+                <v-col align="center" justify="center" cols="12" sm="4">
+                  <v-btn mb="4" size="medium " variant="text" icon="mdi-plus" :color="color" @click="decrement"></v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+
+        <v-col ols="12" sm="6">
+          <v-card title="Vědění" align="center"  color="primary">
+            <v-divider color="secondary"></v-divider>
+            <v-container class="grey lighten-5" >
+              <v-row>
+                <v-col  cols="12" sm="4">
+                  <v-btn  mb="4" size="medium" variant="text" icon="mdi-minus" :color="color" @click="decrement"></v-btn>
+                </v-col>
+                <v-col align="center" justify="center" cols="12" sm="4"><span mb="4"
+                    class="text-h2 font-weight-light mb-4" v-text="atributy"></span></v-col>
+                <v-col align="center" justify="center" cols="12" sm="4">
+                  <v-btn mb="4" size="medium " variant="text" icon="mdi-plus" :color="color" @click="decrement"></v-btn>
                 </v-col>
               </v-row>
             </v-container>
@@ -54,16 +130,13 @@
 
 
 
-      <v-text-field label="Síla"></v-text-field>
-      <v-text-field label="Zručnost"></v-text-field>
-      <v-text-field label="Výdrž"></v-text-field>
-      <v-text-field label="Inteligence"></v-text-field>
-      <v-text-field label="Vědění"></v-text-field>
-      <v-text-field label="šarm"></v-text-field>
 
       <h2>Výbava //TODO</h2>
 
-      <h2>Dodatečné informace</h2>
+      <h2>Role dobrodruha</h2>
+
+      <v-select :items="['LMAO', 'YeetA']" label="Přesvědčení"></v-select>
+      <v-text-field label="Věk"></v-text-field>
       <v-textarea name="input-7-1" label="Další schopnosti"
         value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
         hint="Hint text"></v-textarea>
@@ -85,8 +158,9 @@
 export default {
   data: () => ({
     valid: true,
-    races: ['Anhilarský člověk', 'Grobr', 'Hruurský člověk', 'Ork', 'Inu-im'],
-    classes: ['Kolos', 'Harcovník', 'Hraničář', 'Lupič', 'Čaroděj', 'Vizír'],
+    races: ['Anhilarský člověk', 'Maanský člověk', 'Grobr', 'Inu-im'],
+    classes: ['Bojovník', 'Kněz', 'Lupič', 'Čaroděj'],
+    //classes: ['Kolos', 'Harcovník', 'Hraničář', 'Lupič', 'Čaroděj', 'Vizír'],
     atributy: 10
 
   }),
