bae2a3e3e5cc7d263e4c80e0ee503d5600e22b0c
Martin Lesak <lesak@cis.cz>
2025-01-23 13:15:06 +0100

    Create Adventurer

--- Client/src/pages/AdventurerManagement.jsx
+++ Client/src/pages/AdventurerManagement.jsx
@@ -4,7 +4,7 @@ import AdventurerManagementChoice from '../components/AdventurerManagementChoice
 function AdventurerManagement() {
   return (
     <div>
-      
+      <a onClick={() => document.getElementById('createAdventurerModal').showModal()} className="m-2 uppercase btn btn-outline text-primary hover:bg-primary hover:text-primary hover:border-backdrop">Vytvořit nového</a>
       <AdventurerManagementChoice></AdventurerManagementChoice>
     </div>
   )
