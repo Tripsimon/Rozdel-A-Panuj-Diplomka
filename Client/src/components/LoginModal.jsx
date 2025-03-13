e84296a8d7f7916a908900ee5b8a2c7930c3b1c3
Martin Lešák <47338791+Tripsimon@users.noreply.github.com>
2025-03-13 18:49:29 +0100

    fix login modalu

--- Client/src/components/LoginModal.jsx
+++ Client/src/components/LoginModal.jsx
@@ -50,7 +50,7 @@ function LoginModal() {
                 <h3 className="text-lg font-bold mb-7 text-primary">Přihlášení</h3>
 
                 <input type="text" placeholder="Email" onChange={(event) => { setEmailState(event.target.value); }} className="w-full mb-5 input input-bordered " />
-                <input type="text" placeholder="Heslo" onChange={(event) => { setPasswordState(event.target.value); }} className="w-full mb-5 input input-bordered" />
+                <input type="password" placeholder="Heslo" onChange={(event) => { setPasswordState(event.target.value); }} className="w-full mb-5 input input-bordered" />
                 <div className="modal-action">
                     <form method="dialog">
                         <button className="mx-5 btn text-primary" onClick={() => { prihlaseni() }}>Přihlásit</button>
