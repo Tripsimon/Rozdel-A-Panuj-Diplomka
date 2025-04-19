9311ed982a1ca91420fd8bc36f8a2318482bcd46
Martin Lešák <tripsimon3@gmail.com>
2025-04-19 15:41:42 +0200

    Oprava alert driveru

--- Client/src/components/AlertDriver.jsx
+++ Client/src/components/AlertDriver.jsx
@@ -1,40 +1,28 @@
 import React from 'react'
 
-const returnAlert = (chosenAlert) => {
+function AlertDriver(props) {
 
-    switch (chosenAlert) {
-        case "wrongLogin":
-            return (
-                <div role="alert" className="alert alert-error">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 shrink-0 stroke-current"
-                        fill="none"
-                        viewBox="0 0 24 24">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <span>Error! Task failed successfully.</span>
-                </div>
-            )
+
+ const handleRender = () =>{
+    if(props.alertState == undefined){return}
+    switch (props.alertState.alertTyp) {
+        case "error":
+            return(<div role="alert" className="mb-10 alert alert-error alert-soft">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 stroke-current shrink-0" fill="none" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>{props.alertState.alertText}</span>
+              </div>)
             break;
-
+    
         default:
-
-
-
             break;
     }
+ }
 
-
-
-}
-
-function AlertDriver() {
-    { returnAlert("wrongLogin") }
+  return (
+    handleRender()
+  )
 }
 
 export default AlertDriver
--- Client/src/components/RegisterModal.jsx
+++ Client/src/components/RegisterModal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { useSelector, useDispatch } from 'react-redux';
 import { reduxIsLoggedIn, loginUser } from '../store/userSlice';
+import AlertDriver from './AlertDriver';
 
 function RegisterModal() {
     const isLoggedIn = useSelector(reduxIsLoggedIn)
@@ -12,39 +13,26 @@ function RegisterModal() {
     const [hesloState, setHesloState] = useState("")
 
     //Upozorneni
-    const [alertToggleState, setAlertToggleState] = useState("")
-    const [alertTitleState, setAlertTitleState] = useState("")
-    const [alertTextState, setAlertTextState] = useState("")
+    const [alertState, setAlertState] = useState({
+        showAlert: false,
+        alertTyp: "",
+        alertText: ""
+    })
 
-    const rules = {
-        hesloRule: [
-            value => {
-                if (value?.length > 7) return true
-                return 'Heslo musí obsahovat alespon 8 znaků'
-            },],
-
-        nameRule: [
-            value => {
-                if (value?.length > 3) return true
-                return 'Jméno musí obsahovat alespon 4 znaky'
-            },],
-
-        emailRule: [
-            value => !value || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || 'Formát zadání není E-Mail'
-        ],
-
-        againPassword: [
-            value => {
-                if (value == heslo.value) return true
-                return 'Heslo se neschoduje'
-            }
-        ]
-    }
 
     /**
      * Funkce, která po odeslání požadavku zajistí registraci uživatele
      */
     function register() {
+
+        if(emailState == ""){
+            setAlertState({
+                showAlert: true,
+                alertTyp: "error",
+                alertText: "Všechny položky musí být vyplněny. Prosím zkuste tuto operaci znovu"
+            })
+            return
+        }
 
 
         axios.post(axios.defaults.baseURL + "/uzivatel/registrace", { 'email': emailState, 'prezdivka': prezdivkaState, 'heslo': hesloState })
@@ -64,7 +52,8 @@ function RegisterModal() {
 
                     default:
                         if (queryResponse.status == 201 || queryResponse.data != null) {
-                            dispatch(loginUser({id:queryResponse.data, opravneni: 'uzivatel', name: prezdivkaState }))
+                            dispatch(loginUser({ id: queryResponse.data, opravneni: 'uzivatel', name: prezdivkaState }))
+                            document.getElementById('registerModal').close()
                         }
                         break;
                 }
@@ -76,13 +65,16 @@ function RegisterModal() {
             <div className="modal-box">
                 <h3 className="text-lg font-bold text-primary">Registrace</h3>
                 <div className="mb-10 divider divider-warning"></div>
+                { alertState.showAlert ? <AlertDriver alertState={alertState}></AlertDriver>:""}
+
+                <div className="hidden validator-hint">Enter valid email address</div>
                 <input type="email" onChange={(event) => { setEmailState(event.target.value); }} placeholder="Email" className="w-full mb-5 input input-bordered " />
                 <input type="text" placeholder="Přezdívka" onChange={(event) => { setPrezdivkaState(event.target.value); }} className="w-full mb-5 input input-bordered" />
                 <input type="password" placeholder="Heslo" onChange={(event) => { setHesloState(event.target.value); }} className="w-full mb-5 input input-bordered " />
                 <input type="password" placeholder="Opakovat heslo" className="w-full mb-5 input input-bordered" />
                 <div className="modal-action">
                     <form method="dialog">
-                        <button onClick={() => { register() }} className="mx-5 uppercase btn bg-secondary btn-outline hover:bg-primary hover:text-secondary hover:border-backdrop text-primary">Registrovat</button>
+                        <button type='button' onClick={() => { register() }} className="mx-5 uppercase btn bg-secondary btn-outline hover:bg-primary hover:text-secondary hover:border-backdrop text-primary">Registrovat</button>
                         <button className="uppercase btn bg-secondary btn-outline hover:bg-primary hover:text-secondary hover:border-backdrop text-primary">Zavřít</button>
                     </form>
                 </div>
